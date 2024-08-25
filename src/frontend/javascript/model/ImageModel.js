3ec73835515b16529647dcf11bb8bf2bfcf5371e
Shreshth Verma <verma2007s@gmail.com>
2024-08-25 18:45:02 +0530

    feat: added logic for getting pixel data uploaded image

--- src/frontend/javascript/model/ImageModel.js
+++ src/frontend/javascript/model/ImageModel.js
@@ -1,6 +1,8 @@
 export default class ImageModel {
-  constructor(imageURL) {
-    this._image = new Image(imageURL);
+  constructor() {
+    this._image = new Image();
+    this._canvas = document.createElement("canvas");
+    this._context = this._canvas.getContext("2d");
   }
 
   get image() {
@@ -9,5 +11,26 @@ export default class ImageModel {
 
   set image(imageURL) {
     this._image.src = imageURL;
+
+    this._image.onload = () => {
+      this._drawImage();
+      this._imagePixelData = this._context.getImageData(
+        0,
+        0,
+        this._canvas.width,
+        this._canvas.height
+      ).data
+    };
+  }
+
+  _drawImage() {
+    this._canvas.width = this._image.width;
+    this._canvas.height = this._image.height;
+
+    this._context.drawImage(this._image, 0, 0);
+  }
+
+  get imagePixelData() {
+    return this._imagePixelData;
   }
 }
